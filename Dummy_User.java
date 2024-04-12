509af5ea46e1a478274fd2cd48dcf8fe44be5fcf
penelopegr <95437888+penelopegr@users.noreply.github.com>
2024-04-12 17:26:07 +0300

    Add files via upload

--- Dummy_User.java
+++ Dummy_User.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.net.*;
-import java.util.List;
 import java.util.Scanner;
 
 public class Dummy_User extends Thread {
@@ -8,9 +7,6 @@ public class Dummy_User extends Thread {
 	String Type;//Manager or Client
 	String received;
 	Scanner scanner = new Scanner(System.in);
-    Pair<Integer, List<Room>> finalresult;
-    Socket reducerSocket = null;
-    ObjectInputStream reducerIn = null;
 
 	
 	public void run() {
@@ -25,11 +21,11 @@ public class Dummy_User extends Thread {
             //while (true) {
                 /* Create socket for contacting the server on port 4444 */
                 socket = new Socket("localhost", 12345);
-                System.out.println(socket);
+
                 /* Create the streams to send and receive data from server */
                 out = new ObjectOutputStream(socket.getOutputStream());
-                
-                
+                //in = new ObjectInputStream(socket.getInputStream());
+
                 System.out.println("Choose: Manager/Client");
                 Type = scanner.nextLine();
 
@@ -62,7 +58,9 @@ public class Dummy_User extends Thread {
                     answer = scanner.nextLine();
                     
                     if (answer.equals("1")) {
-                        System.out.println("Describe your filter as: Location,Available Dates,Number of people,Price,Stars");
+                        System.out.println("Describe your filter as: Location,First Day Of Stay, Last Day Of Stay, Number of people,Price,Stars");
+                        System.out.println("Date format should be yyyy/mm/dd.");
+                        System.out.println("If you don't want to add a filter, add an x on that field. e.g. Naxos,x,x,x,x,x");
                         RoomDetails = scanner.nextLine();
                         
                     }else if(answer.equals("2")){
@@ -85,19 +83,7 @@ public class Dummy_User extends Thread {
                 out.flush();
                 out.writeUTF(RoomDetails); // Details for filtering
                 out.flush();
-
-                in = new ObjectInputStream(socket.getInputStream());
-                System.out.println(in.readUTF());
-
-                // Connect to the ReducerHandler
-               // reducerSocket = new Socket("localhost", finalresult.getKey());
-                //reducerIn = new ObjectInputStream(reducerSocket.getInputStream());
-
-                // Listen for results from the ReducerHandler
-               // Pair<Integer, List<Room>> reducerResult = (Pair<Integer, List<Room>>) reducerIn.readObject();
-                //System.out.println(reducerResult);
-
-             // }
+          //  }
 
         } catch (UnknownHostException unknownHost) {
 			System.err.println("You are trying to connect to an unknown host!");
