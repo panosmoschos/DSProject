f0cf4030916c50e643eeaef9401241004c0a9fdf
penelopegr <95437888+penelopegr@users.noreply.github.com>
2024-04-16 17:02:53 +0300

    minor fix 2

--- Room.java
+++ Room.java
@@ -16,6 +16,7 @@ public class Room implements Serializable {
     private int price;
     private List<Available_Date> availability;
     private String owner;
+    private List<Booking> bookings;
 
     // Constructor
     public Room(String roomName, int noOfPersons, String area, int stars, int noOfReviews, String roomImage, int price, List<Available_Date> availability, String owner) {
@@ -28,15 +29,14 @@ public class Room implements Serializable {
         this.price = price;
         this.availability = availability;
         this.owner = owner;
+        this.bookings = new ArrayList<>();
     }
    
-    // for testing
     public List<Available_Date> getAvailability() {
         return availability;
     }
 
-    // for testing
-    public static String getOwner(Room room){
+    public String getOwner(Room room){
         return room.owner;
     }
 
@@ -51,8 +51,24 @@ public class Room implements Serializable {
     public int getStars(){
         return stars;
     }
+    public int getNoPerson(){
+        return noOfPersons;
+    }
+
+    public int getNoReviews(){
+        return noOfReviews;
+    }
+    public int getPrice(){
+        return price;
+    }
+
+    public String getImage(){
+        return roomImage;
+    }
 
 
+
+    // (Client) Adds booking
     public void addBooking(Request req){
         // looks like [roomName,FirstDayOfStay,LastDayOfStay]
         String[] details = req.details.split(","); 
@@ -67,14 +83,47 @@ public class Room implements Serializable {
 
         if (wanted.isAvailable(availability)){
             availability = wanted.RemoveFrom(availability);
+            bookings.add(new Booking(wanted,roomName,area));
         }else{
             System.out.println("Sorry, this date is not available at " + details[0] + ".");
         }
     }
 
+    // (Manager) returns list of bookings of area within two dates
+    public List<Booking> getAreaBookingsBetween(Request req){
+        String[] details = req.details.split(",");
+        // looks like [area,FromThisDay,TillThatDay]
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDate MIN = LocalDate.parse(details[1],df);
+        LocalDate MAX = LocalDate.parse(details[2],df);
+        
+        List<Booking> between = new ArrayList<>();
+        
+        for (Booking booking : bookings){
+
+            boolean af_eq_MIN = booking.DateOfStay.getFirstDay().isAfter(MIN) || booking.DateOfStay.getFirstDay().isEqual(MIN);
+            boolean bef_eq_MAX = booking.DateOfStay.getLastDay().isBefore(MAX) || booking.DateOfStay.getLastDay().isEqual(MAX);
+            boolean rightArea = details[0].equals(booking.getArea());
+
+            if (af_eq_MIN && bef_eq_MAX && rightArea){
+                between.add(booking);
+            }
+        }
+
+        return between;
+    }
+
+    // (Manager) Returns bookings of owner
+    public List<Booking> getOwnerBookings(Request req){
+        if (req.details.equals(owner)){
+            return bookings;
+        }
+        return new ArrayList<>();
+    }
+
     // (Manager) Adds new available dates
     public void addAvailability(Request req){
-
         String[] details = req.details.split(",");
         List<Available_Date> newDates = new ArrayList<>();
         DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy/MM/dd");
@@ -120,7 +169,7 @@ public class Room implements Serializable {
         }
     }
 
-    // Changes the rating and the noOfReviews of the room
+    // (Client) Changes the rating and the noOfReviews of the room
     public void ratingChanges(Request req){
         String[] details = req.details.split(",");
         int newStarRating = Integer.parseInt(details[1]);
@@ -128,8 +177,8 @@ public class Room implements Serializable {
         stars = (stars * (noOfReviews - 1) + newStarRating) / noOfReviews;
     }
 
-    // Filters a list of rooms
-    public List<Room> filterRooms(List<Room> givenRooms, Request req) {
+    // (Client) Filters a list of rooms
+    public static List<Room> filterRooms(List<Room> givenRooms, Request req) {
         List<Room> matchingRooms = new ArrayList<>();
 
         String[] arrayFilter = req.details.split(",");
@@ -202,6 +251,7 @@ public class Room implements Serializable {
         return null;
     }
 
+    // (Initialization) Reads and returns a list of rooms from a path
     public static List<Room> roomsOfFolder(String path) {
         File dir = new File(path);
         File[] files = dir.listFiles();
@@ -250,10 +300,75 @@ public class Room implements Serializable {
     }
 
 
+    // (Client) Reads and returns a list of rooms from a path
+    public static Room addRoom(Request req) {
+        String[] details = req.details.split(",");
+        String path = details[1];
+        File f = new File(path);
+
+        Room room = new Room("x", 0, path, 0, 0, path, 0, null, path);
+
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader(f));
+            JSONObject JSONobj= (JSONObject) obj;
+
+            String roomName = (String) JSONobj.get("roomName");
+            int noOfPersons = ((Long) JSONobj.get("noOfPersons")).intValue();
+            String area = (String) JSONobj.get("area");
+            int stars = ((Long) JSONobj.get("stars")).intValue();
+            int noOfReviews = ((Long) JSONobj.get("noOfReviews")).intValue();
+            String roomImage = (String) JSONobj.get("roomImage");
+            int price = ((Long) JSONobj.get("price")).intValue();
+            String owner = (String) JSONobj.get("owner");
+
+            List<Available_Date> dates = new ArrayList<>();
+
+            JSONArray available_dates = (JSONArray) JSONobj.get("availability");
+            for (Object Obj : available_dates){
+                JSONObject jsonOB = (JSONObject) Obj;
+                String FirstDayString = (String) jsonOB.get("start_date");
+                String LastDayString = (String) jsonOB.get("end_date");
+                    
+                DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+                LocalDate FirstDayDate = LocalDate.parse(FirstDayString,df);  
+                LocalDate LastDayDate =  LocalDate.parse(LastDayString,df);
+                    
+
+                dates.add(new Available_Date(FirstDayDate,LastDayDate));
+            }
+
+            room = new Room(roomName, noOfPersons, area, stars, noOfReviews, roomImage, price, dates, owner);
+
+
+        }catch (Exception e){
+            System.out.println("Exception:" + e);
+        }   
+        
+        showRoom(room);
+        return room;
+    }
+
+    public static void showRoom(Room room) {
+        System.out.println("Room Name: " + room.getRoomName() +
+                           "\nNo. of Persons: " + room.getNoPerson() +
+                           "\nArea: " + room.getArea(room) +
+                           "\nStars: " + room.getStars() +
+                           "\nNo. of Reviews: " + room.getNoReviews() +
+                           "\nRoom Image:" + room.getImage() +
+                           "\nPrice: " + room.getPrice() +
+                           "\nAvailability: ");
+        for (Available_Date ad : room.getAvailability()){
+            System.out.println("\t" + ad.getTimePeriod());
+        }
+        System.out.println("Owner: " + room.getOwner(room));
+    }
+    
+
+
     // TESTING
     public static void main(String[] args) {
-        String folderPath = "bin/rooms";
-        List<Room> rooms = roomsOfFolder(folderPath);
+        String folderPath = "bin/rooms/Anatoli.json";
 
         /* TESTING addAvailability 
 
