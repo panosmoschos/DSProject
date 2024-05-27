9426c062807b9ec327f4a9d20f759c100a49095e
Faidonas <ff.azariadis@gmail.com>
2024-05-27 18:41:47 +0300

    New design

---
+++ Android/app/src/main/java/com/example/myapplication/ResultsActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_results);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // sto details tha mpei h lista me ta dwmatia
+        RecyclerView roomsRecycler = findViewById(R.id.roomsRecyclerView);
+        roomsRecycler.setLayoutManager(new LinearLayoutManager(this));
+        //roomsRecycler.setAdapter(new RoomsAdapter(this,details));
+
+    }
+}
